073ee73ced5cd5620f0a1a363ea38c7d7b98247c
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 03:22:13 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowkey = nil;
+  lowval = nil;
+  name_hash.each do |k, v|
+    if lowkey == nil || v < lowval
+      lowval = v;
+      lowkey = k;
+    end
+  end
+  lowkey
 end
