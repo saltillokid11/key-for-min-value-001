5e2464cdb9e632e0c1117a39390514898c083265
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 03:19:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  mymin = nil;
-  name_hash.each do |name|
-    puts "name: " + :name
-    # puts "mymin: " + mymin
-    # if mymin < :name
-    #   :name = mymin
-    # end
+  lowkey = nil;
+  lowval = nil;
+  name_hash.each do |k, v|
+    
   end
 end
